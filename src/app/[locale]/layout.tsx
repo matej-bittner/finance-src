7b2c546edbda423a702e08326dd48d00d21bb533
Matej Bittner <mates-bittner@seznam.cz>
2024-07-21 10:57:34 +0200

    almost done14

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,6 +15,13 @@ export const metadata: Metadata = {
   description: "Manage your money - effortlessly.",
   twitter: {
     card: "summary_large_image",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
   openGraph: {
     title: "Wallet Recap",
@@ -22,6 +29,13 @@ export const metadata: Metadata = {
     type: "website",
     url: `${process.env.NEXT_PUBLIC_APP_URL}`,
     siteName: "WalletRecap",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
 };
 
